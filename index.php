15da02e67c544bd25c93f1e22a8d08c5a0e4c943
Rod Jacka <rodj@panalysis.com>
2020-07-04 14:38:51 +1000

    Added measurement protocol

--- index.php
+++ index.php
@@ -1,7 +1,228 @@
-<!DOCTYPE HTML>
 <?php
+
+abstract class AbstractAnalytics {
+
+    protected $debug = false;
+
+    public function __construct($debug) {
+          $this->debug = $debug;
+    }
+
+    /*
+    Sends a normal page-view type tracking request to the analytics server
+    */
+    public function Track($title) {
+        if (!method_exists($this, "GetHitRequest")) {
+            throw new Exception( "Missing GetHitRequest function");
+        }
+
+        $response = $this->_URLPost(
+                    $this->getHost(), 
+                    $this->GetHitRequest($this->getUrlPath(), 
+                                         $title));
+        if( $this->debug )
+            echo $response;
+        return $response;
+    }
+
+    /*
+    Sends a exception type tracking request to the analytics server
+    */
+    public function Error($title, $errorcode) {
+        if (!method_exists($this, "GetErrorRequest")) {
+            throw new Exception( "Missing GetErrorRequest function");
+        }
+
+        $response = $this->_URLPost(
+                    $this->getHost(), 
+                    $this->GetErrorRequest($this->getUrlPath(), 
+                                           $title, 
+                                           $errorcode));
+        if( $this->debug )
+            echo $response;
+        return $response;
+    }
+
+    /*
+    Gets the analytics host name (e.g. https://www.google-analytics.com)
+    */
+    abstract protected function getHost();
+
+    /*
+    Gets the full url to the requested resource
+    */
+    protected function getUrlPath() {
+        return 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . 
+           '://' . 
+           "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    }
+
+    /*
+    Gets the user agent attached to the original request
+    */
+    protected function _getUserAgent() {
+        return array_key_exists('HTTP_USER_AGENT', $_SERVER) 
+            ? $_SERVER['HTTP_USER_AGENT'] : "";
+    }
+
+    /*
+    Gets the http referer for the original request
+    */
+    protected function _getReferer() {
+        return array_key_exists('HTTP_REFERER', $_SERVER) 
+          ? $_SERVER['HTTP_REFERER'] : "";
+    }
+
+    /* 
+    Gets the remote ip address for the original request
+    */
+    protected function _getRemoteIP() {
+        return array_key_exists('REMOTE_ADDR', $_SERVER) 
+          ? $_SERVER['REMOTE_ADDR'] : "";
+    }
+
+    /*
+    Performs a POST request of the data in $data_array to the URL in $url
+    */
+    private function _URLPost($url, $data_array) { 
+      // Need to encode spaces, otherwise services such
+      // as Google will return 400 bad request!
+      $url = str_replace(" ", "%20", $url);
+
+      // Construct the contexts for the POST requests
+      $opts = array(
+        'https'=>array(
+        'method'=>"POST",
+        'header'=>
+          "Accept: application/json, text/javascript, */*; q=0.01\r\n".
+          "Content-type: application/x-www-form-urlencoded; charset=UTF-8\r\n".
+          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36\r\n".
+          "Referer: https://api.example.com/\r\n",
+        'content' => http_build_query($data_array)
+        )
+        ,
+        'http'=>array(
+        'method'=>"POST",
+        'header'=>
+          "Accept: application/json, text/javascript, */*; q=0.01\r\n".
+          "Content-type: application/x-www-form-urlencoded; charset=UTF-8\r\n".
+          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36\r\n".
+          "Referer: https://api.example.com/\r\n",
+        'content' => http_build_query($data_array)
+        )
+      );
+
+      $context = stream_context_create($opts);
+      $result = null;
+      $dh = fopen("$url",'rb', false, $context);
+      if( !$dh )
+        return null;
+
+      if( $dh !== false )
+        $result = stream_get_contents($dh);
+
+      fclose($dh);
+
+      return $result; 
+  }
+}
+
+class GoogleAnalytics extends AbstractAnalytics {
+
+    protected $google_host = 'https://www.google-analytics.com/collect';
+    protected $google_debug_host = 'https://www.google-analytics.com/debug/collect';
+
+    /* 
+    The Google Analytics Tracking Id for this property (e.g. UA-XXXXXX-XX)
+    */
+    protected $trackingId = '';
+
+    /*
+    The name of the application, this is sent to the Google servers
+    */
+    protected $appName = '';
+
+    public function __construct($TrackingID, $ApplicationName, $debug) {
+        parent::__construct($debug);
+
+        $this->trackingId = $TrackingID;
+        $this->appName = $ApplicationName;
+    }
+
+    protected function getHost() {
+      if( $this->debug )
+          return $this->google_debug_host;
+      return $this->google_host;
+    }
+
+    private function getCommonDataArray($url, $title){
+        // Standard params
+        $v   = 1;
+        $cid = $this->_ParseOrCreateAnalyticsCookie();
+
+        return array(
+            'v'   => $v,
+            'tid' => $this->trackingId,
+            'cid' => $cid,
+            'an'  => $this->appName,
+            'dt'  => $title,
+            'dl'  => $url, 
+            'ua'  => $this->_getUserAgent(),
+            'dr'  => $this->_getReferer(),
+            'uip' => $this->_getRemoteIP(),
+            'av'  => '1.0'
+        );
+    }
+
+    protected function GetHitRequest($url, $title) {
+        // Create the pageview data
+        $data = $this->getCommonDataArray($url, $title);
+        $data['t'] = 'pageview';
+
+        // Send PageView hit as POST
+        return $data;
+    }
+
+    protected function GetErrorRequest($url, $title, $errorcode){
+        // Create the error data
+        $data = $this->getCommonDataArray($url, $title);
+        $data['t']   = 'exception';
+        $data['exd'] = $errorcode;
+        $data['exf'] = '1';
+
+        return $data;
+    }
+
+  // Gets the current Analytics session identifier or 
+  // creates a new one if it does not exist
+    private function _ParseOrCreateAnalyticsCookie() {
+      if (isset($_COOKIE['_ga'])) {
+          // An analytics cookie is found
+          list($version, $domainDepth, $cid1, $cid2) = preg_split('[\.]', $_COOKIE["_ga"], 4);
+          $contents = array(
+            'version' => $version,
+            'domainDepth' => $domainDepth,
+            'cid' => $cid1 . '.' . $cid2
+          );
+          $cid = $contents['cid'];
+      } else {
+          // no analytics cookie is found. Create a new one
+          $cid1 = mt_rand(0, 2147483647);
+          $cid2 = mt_rand(0, 2147483647);
+
+          $cid = $cid1 . '.' . $cid2;
+          setcookie('_ga', 'GA1.2.' . $cid, time() + 60 * 60 * 24 * 365 * 2, '/');
+      }
+      return $cid;
+    }
+}
+
 $gtm = filter_var($_GET['gtm'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_LOW);
+
 ?>
+
+
+<!DOCTYPE HTML>
 <html>
 	<head>
 		<title>Panalysis Skills Test - Basic Analytics and tagging</title>
@@ -164,6 +385,10 @@ Your GTM ID: <?php print($gtm); ?>
 					</footer>
 
 			</div>
+<?php
+$analytics = new GoogleAnalytics("UA-4077168-15", "My API", false);
 
+$analytics->Track("GTM Test");
+?>
 	</body>
 </html>
